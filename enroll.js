00bfc42e8a2fc3cf2d77ad3fa821c7cbd6088789
mypages090309-ops <mypages.090309@gmail.com>
2026-03-24 16:59:05 +0800

    Update enroll.js

--- enroll.js
+++ enroll.js
@@ -31,7 +31,7 @@ document.getElementById("enrollForm").addEventListener("submit", async function(
   };
 
   try {
-    // SEND TO CLOUDFARE WORKER
+    // SEND TO WORKER
     const res = await fetch("https://enrollmentworker.q2296439.workers.dev/", {
       method: "POST",
       headers: {
@@ -43,11 +43,26 @@ document.getElementById("enrollForm").addEventListener("submit", async function(
     const result = await res.json();
 
     if (result.success) {
-      document.getElementById("successMsg").innerText =
-        "✅ Successfully enrolled! Welcome, " + username;
+
+      let seconds = 5;
+
+      const msg = document.getElementById("successMsg");
+
+      const interval = setInterval(() => {
+        msg.innerText =
+          `✅ Successfully enrolled! Welcome, ${username} | Redirecting in ${seconds}s...`;
+
+        seconds--;
+
+        if (seconds < 0) {
+          clearInterval(interval);
+          window.location.href = "login.html";
+        }
+      }, 1000);
 
       // RESET FORM
       document.getElementById("enrollForm").reset();
+
     } else {
       alert("❌ Failed to save data.");
     }
