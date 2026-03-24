b0140bf018e71e1b45ea874937a2e9c719647332
mypages090309-ops <mypages.090309@gmail.com>
2026-03-24 16:34:43 +0800

    Remove enrollment form submission and add goBack function

--- enroll.js
+++ enroll.js
@@ -1,12 +1,3 @@
-document.getElementById("enrollForm").addEventListener("submit", function(e){
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const course = document.getElementById("course").value;
-
-  document.getElementById("successMsg").innerText =
-    "✅ Successfully enrolled, " + name + " (" + course + ")";
-
-  this.reset();
-});
+function goBack() {
+  window.location.href = "index.html";
+}
